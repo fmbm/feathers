989d8f1cfd7c07442a4444da970c0297ea44d0a3
prescottprue <sprue.dev@gmail.com>
2019-04-06 17:09:38 -0700

    Add size-limit.

---
+++ packages/react-feathers/.size-limit.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    path: "dist/index.js",
+    name: "Base Index File",
+    webpack: false,
+    limit: "150 B"
+  },
+  {
+    path: "dist/AutoComplete/index.js",
+    name: "AutoComplete Component",
+    webpack: false,
+    limit: "68 B"
+  }
+]
