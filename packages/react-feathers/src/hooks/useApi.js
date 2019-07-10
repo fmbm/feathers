6dddc824f1c125ebb04a168909284d218fe8fc73
Gio Logist <gio@giologist.com>
2019-07-10 11:34:11 -0700

    onError support for SearchBox

--- packages/react-feathers/src/hooks/useApi.js
+++ packages/react-feathers/src/hooks/useApi.js
@@ -1,14 +1,14 @@
-import { useEffect, useContext, useState, useDebugValue } from 'react'
-import FeathersContext from '../FeathersContext'
+import { useEffect, useContext, useState, useDebugValue } from "react"
+import FeathersContext from "../FeathersContext"
 
-function useApi({ serviceName, params }, ready = true) {
+function useApi({ serviceName, params, onError }, ready = true) {
   const feathers = useContext(FeathersContext)
 
   const [response, setResponse] = useState()
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState()
 
-  useDebugValue('Hello')
+  useDebugValue("Hello")
 
   useEffect(() => {
     if (ready) {
@@ -18,12 +18,13 @@ function useApi({ serviceName, params }, ready = true) {
           .find(params)
           .then(response => setResponse(response))
           .catch(err => {
-            console.error('Error calling Feathers API:', err) // eslint-disable-line no-console
+            if (onError) onError(err)
+            console.error("Error calling Feathers API:", err) // eslint-disable-line no-console
             return Promise.reject(err)
           })
           .finally(setLoading(false))
       } catch (e) {
-        console.error('Call to Feathers API threw an error:', e) // eslint-disable-line no-console
+        console.error("Call to Feathers API threw an error:", e) // eslint-disable-line no-console
         setError(e)
         setLoading(false)
       }
