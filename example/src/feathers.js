736b3d43e94f47d62fb2697fb76b971c011a2143
Gio Logist <gio@giologist.com>
2020-08-05 08:46:23 -0700

    Use env variable for port

--- example/src/feathers.js
+++ example/src/feathers.js
@@ -2,6 +2,6 @@ import feathers from "@feathersjs/feathers"
 import rest from "@feathersjs/rest-client"
 
 const client = feathers()
-client.configure(rest("http://localhost:3030").fetch(window.fetch))
+client.configure(rest(`http://localhost:${process.env.REACT_APP_FEATHERS_PORT}`).fetch(window.fetch))
 
 export default client
